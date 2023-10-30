6033e428ea73bfe75db8527ee752c8fb588cdd51
devebe <dvbirdja@gmail.com>
2023-10-30 23:07:52 +0100

    Build logic for footer

--- src/data/data.js
+++ src/data/data.js
@@ -1,6 +1,29 @@
+export const restaurantName = 'Restaurant Nacho';
+
 export const tabListData = [];
 tabListData[0] = 'Menu';
 tabListData[1] = 'Events';
 tabListData[2] = 'Contact';
+tabListData[3] = 'Careers';
 
-export const restaurantName = 'De Kleine Zondaar';
+export const footerListData = [];
+footerListData[0] = {
+    id: 1,
+    text: 'About us',
+    href: '#',
+};
+footerListData[1] = {
+    id: 2,
+    text: 'Privacy policy',
+    href: '#',
+};
+footerListData[2] = {
+    id: 3,
+    text: 'Terms and Conditions',
+    href: '#',
+};
+footerListData[3] = {
+    id: 4,
+    text: 'Cookie settings',
+    href: '#',
+};
