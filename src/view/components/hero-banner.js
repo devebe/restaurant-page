298afacbc9ba1282c7c3953d4218da9872bc5724
devebe <dvbirdja@gmail.com>
2023-10-31 20:53:26 +0100

    Additions to hero banner component

--- src/view/components/hero-banner.js
+++ src/view/components/hero-banner.js
@@ -1,2 +1,16 @@
 import Component from "./component-class";
 
+export const renderHeroBanner = () => {
+    const banner = new Component('banner','div');
+    banner.build();
+
+    const heroTitle = new Component('heroTitle','h1');
+    heroTitle.parent = document.getElementById(banner.id);
+    heroTitle.build();
+    heroTitle.addText('Where Louisiana and The Netherlands meet');
+
+    const heroSubTitle = new Component('heroSubTitle','h3');
+    heroSubTitle.parent = document.getElementById(banner.id);
+    heroSubTitle.build();
+    heroSubTitle.addText('Authentic Cajun cooking mixed with Dutch Design');
+}
