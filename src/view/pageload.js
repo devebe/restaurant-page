8e75ce08d0e379e5525e17896a33ff71dedda445
devebe <dvbirdja@gmail.com>
2023-10-31 20:28:02 +0100

    Change to way in which page reacts to click on tab

--- src/view/pageload.js
+++ src/view/pageload.js
@@ -1,5 +1,6 @@
 import { renderHeader } from "./header";
 import { renderFooter } from "./footer";
+import { restaurantName } from "../data/common";
 import { tabListData } from "../data/header";
 
 export default function buildPage(tabEvent) {
@@ -9,20 +10,28 @@ export default function buildPage(tabEvent) {
 }
 
 const renderContent = (tabEvent) => {
-    if (!tabEvent) {
-        console.log('No tabevent specified: Homepage is created');
+    if (!tabEvent || tabEvent == restaurantName) {
+        console.log('One tabevent specified: Homepage is created');
+        // renderHomePage()
     };
     if (tabEvent == tabListData[0].text) {
         console.log('One tabevent specified: Menu page is created');
         setBtnToActive(`tabListBtn ${tabListData[0].id}`);
+        // renderMenuPage()
     };
     if (tabEvent == tabListData[1].text) {
         console.log('One tabevent specified: Events page is created');
         setBtnToActive(`tabListBtn ${tabListData[1].id}`);
+        // renderEventsPage()
+    };
+    if (tabEvent == tabListData[2].text) {
+        console.log('One tabevent specified: Contact page is created');
+        setBtnToActive(`tabListBtn ${tabListData[2].id}`);
+        // renderContactPage()
     };
 };
 
 const setBtnToActive = (id) => {
     let target = document.getElementById(id);
     target.classList.add('active');
-}
+};
