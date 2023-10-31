95ff28d11efdb7c27715e45b8c114f513c81ae42
devebe <dvbirdja@gmail.com>
2023-10-31 19:59:43 +0100

    Set up event for switching tabs

--- src/index.js
+++ src/index.js
@@ -2,3 +2,8 @@ import './style.css';
 import buildPage from './view/pageload';
 
 buildPage();
+
+let tabs = document.getElementById('tabList');
+tabs.addEventListener('click',(e) => {
+    console.log(e.target.firstChild.textContent);
+});
--- src/view/pageload.js
+++ src/view/pageload.js
@@ -1,7 +1,18 @@
 import { renderHeader } from "./header";
 import { renderFooter } from "./footer";
+import { tabListData } from "../data/header";
 
-export default function buildPage() {
+export default function buildPage(tabEvent) {
     renderHeader();
+    renderContent(tabEvent);
     renderFooter();
 }
+
+const renderContent = (tabEvent) => {
+    if (!tabEvent) {
+        console.log('No tabevent specified: Homepage is created');
+    };
+    if (tabEvent == tabListData[0].text) {
+        console.log('One tabevent specified: Menu page is created');
+    };
+};
