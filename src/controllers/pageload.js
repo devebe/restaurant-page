1f023dc9a873e66ae118a7c866100ddef1ef4506
devebe <dvbirdja@gmail.com>
2023-10-31 20:34:11 +0100

    Remove imports in pageload

--- src/controllers/pageload.js
+++ src/controllers/pageload.js
@@ -1,8 +1,6 @@
 import { renderHeader } from "../view/header";
-import { renderContent } from "./pagecontent"
+import { renderContent } from "./pagecontent";
 import { renderFooter } from "../view/footer";
-import { restaurantName } from "../data/common";
-import { tabListData } from "../data/header";
 
 export default function buildPage(tabEvent) {
     renderHeader();
